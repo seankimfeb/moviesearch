ac6ec76f4df96b180c1f64d49d98fe85585c09eb
seankimfeb <seankimfeb@gmail.com>
2023-06-26 13:24:08 +0900

    added gson

--- src/main/java/com/example/moviesearch/entity/Movie.java
+++ src/main/java/com/example/moviesearch/entity/Movie.java
@@ -1,11 +1,14 @@
 package com.example.moviesearch.entity;
 
+import lombok.Getter;
+
+@Getter
 public class Movie {
-    String title;
-    Integer year;
-    String imdbID;
-    String type;
-    String poster;
+    String Title;
+    Integer Year;
+    String ImdbID;
+    String Type;
+    String Poster;
     
 //{"Search":
 //[{"Title":"Parasite","Year":"2019","imdbID":"tt6751668","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"},{"Title":"Parasite","Year":"1982","imdbID":"tt0084472","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTFlZDVjMDMtODkwNS00MTM3LWJiMzQtY2IxN2JiNWZjMWUxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"}
--- src/main/java/com/example/moviesearch/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/moviesearch/service/impl/UserServiceImpl.java
@@ -1,25 +1,38 @@
 package com.example.moviesearch.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
+import com.example.moviesearch.clients.MovieApiClient;
 import com.example.moviesearch.dto.MovieResponseDto;
 import com.example.moviesearch.entity.Movie;
+import com.google.gson.Gson;
 
 public class UserServiceImpl {
-    List<Movie> searchMovie(MovieResponseDto dto){
+    //List<Movie> searchMovie(MovieResponseDto dto){
 
-    }
+    //}
 
 
-    private Movie extractMovieFromJson(String content){
-        Movie movieResult = new Movie();
+    private static Movie extractMovieFromJson(String content){
+        
         String title;
         Integer year;
         String imdbID;
         String type;
         String poster;
-
-
+        Gson gson = new Gson();
+       // Map jsonJavaRootObject = new Gson().fromJson(content, Map.class);
+        //    System.out.println(jsonJavaRootObject.get("Title"));
+        Movie movieResult = gson.fromJson(content,Movie.class);
+        return movieResult;
+    }
+    public static void main(String[] args) {
+        String parasite = (MovieApiClient.searchMovieByTitle("Aqua Teen Hunger Force Colon","9025dd4f"));
+    System.out.println(parasite);
+    System.out.println("Extracted:\n");
+    Movie movie = extractMovieFromJson(parasite);
+    System.out.println(movie.getTitle());
     }
     
 }
