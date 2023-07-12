b538f96a03c155153e924d59a35c9e5e23859a15
seankimfeb <seankimfeb@gmail.com>
2023-07-13 06:59:58 +0900

    search works now

--- src/main/java/com/example/moviesearch/controllers/IndexController.java
+++ src/main/java/com/example/moviesearch/controllers/IndexController.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moviesearch.clients.MovieApiClient;
 import com.example.moviesearch.entity.MovieList;
@@ -25,7 +29,18 @@ public class IndexController {
     MovieList ml = new MovieList();
     ml.setSearch(UserServiceImpl.extractMovieFromJson(json));
     model.addAttribute("result", ml.toString());
+    model.addAttribute("size",ml.getSearch().size());
     return "search";
   }
-
+  @RequestMapping(value="/search", method= RequestMethod.POST)
+  public String takeInput(@RequestParam("movie1") String movieTitle, Model model){
+     String json = MovieApiClient.searchMovieByTitle(movieTitle,"9025dd4f");
+     MovieList ml = new MovieList();
+     ml.setSearch(UserServiceImpl.extractMovieFromJson(json));
+     model.addAttribute("result", ml.toString());
+    model.addAttribute("size",ml.getSearch().size());
+    return "search";
+  }
+//https://stackoverflow.com/questions/57744114/how-to-show-list-of-object-in-thymeleaf-in-spring-boot-project
+//https://strongstar.tistory.com/16
 }
---
+++ src/main/java/com/example/moviesearch/controllers/RequestMappingMapping.java
@@ -0,0 +1,5 @@
+package com.example.moviesearch.controllers;
+
+public @interface RequestMappingMapping {
+
+}
