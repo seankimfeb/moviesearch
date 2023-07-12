5cd15cc56382747852dcf19cba740f9487e49b4c
seankimfeb <seankimfeb@gmail.com>
2023-07-12 10:59:10 +0900

    addattribute added

--- src/main/java/com/example/moviesearch/controllers/IndexController.java
+++ src/main/java/com/example/moviesearch/controllers/IndexController.java
@@ -3,8 +3,11 @@ package com.example.moviesearch.controllers;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.moviesearch.clients.MovieApiClient;
 import com.example.moviesearch.entity.MovieList;
 import com.example.moviesearch.service.impl.UserServiceImpl;
 
@@ -15,11 +18,14 @@ public class IndexController {
     public String index(){
 		return "index";
 	}
+  @ModelAttribute
   @GetMapping("/search")
-  public MovieList search(String keyword){
+  public String search(String keyword, Model model){
+    String json = MovieApiClient.searchMovieByTitle("titanic","9025dd4f");
     MovieList ml = new MovieList();
-    ml.setSearch(UserServiceImpl.extractMovieFromJson(keyword));
-    return ml;
+    ml.setSearch(UserServiceImpl.extractMovieFromJson(json));
+    model.addAttribute("result", ml.toString());
+    return "search";
   }
 
 }
